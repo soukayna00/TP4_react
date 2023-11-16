b78c8ab8a1a84df8e65f7af8e46c5637fef2e3fd
soukayna00 <soukaynazaidi06@gmail.com>
2023-11-16 23:52:23 +0100

    the end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-
-
-
-//    generate random  meals 
-
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css';
 function App() {
     const [meal, setMeal] = useState([]);
 
     useEffect(() => {
-        axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+        axios
+            .get('https://www.themealdb.com/api/json/v1/1/random.php')
             .then((result) => {
                 setMeal(result.data.meals);
             })
@@ -19,14 +15,15 @@ function App() {
                 console.log('An error occurred: ', err);
             });
     }, []);
-
     return (
-        <div className='App'>
+        <div className='container App'> {/* Apply container-fluid class here */}
             {meal.map((mealdata, index) => (
                 <div className='meal_holder' key={index}>
                     <img className='image_meal' src={mealdata.strMealThumb} alt='Randommeal' />
                     <h1>{mealdata.strMeal} </h1>
-                <h3><i>{mealdata.strCategory}</i>  Origin : {mealdata.strArea}</h3>
+                    <h3>
+                        <i>{mealdata.strCategory}</i> Origin : {mealdata.strArea}
+                    </h3>
                     <p>{mealdata.strInstructions}</p>
                 </div>
             ))}
@@ -36,47 +33,6 @@ function App() {
 
 export default App;
 
-
-
-//___________tp de pagination 
-
-// import axios from 'axios';
-// import { useEffect } from 'react';
-// import { useState } from 'react';
-// import './App.css'
-
-// function Api(){
-//   const [todo, settodo] = useState([]);
-//   const [page,setpage]=useState(1)
-
-//   useEffect(() => {
-//     axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
-//       .then(result=>settodo(result.data))
-//       .catch(error=>console.error(error))
-//   },[page])
-
-//   const handlePrevious=()=>{
-//     if (page>1){
-//       setpage(page-1)
-//     }
-//   }
-//   const handleNext=()=>{
-//      setpage(page+1)
-//   }
-// return(
-//     <div className="App">
-//       {todo.map((elemt,index)=>(
-//         <div className='holder' key={index}>
-//           <h2>Id {elemt.id}</h2>
-//           <h2>Title {elemt.title}</h2>
-//         </div>
-//       ))}
-//      <button onClick={handlePrevious}>previous</button>
-//      <button onClick={handleNext}>next</button>
-//     </div>
-// ) 
-// }
-// export default Api
 
 
 
